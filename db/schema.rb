7457b447d211a788466f33dec5ae1baa12a149f9
Chris Bedoya <christian.bedoya@yahoo.com>
2015-09-19 12:07:12 -0400

    september 19, 2015

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150915000736) do
+ActiveRecord::Schema.define(version: 20150919155735) do
 
   create_table "products", force: true do |t|
     t.string   "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150915000736) do
     t.string   "category"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
 end
