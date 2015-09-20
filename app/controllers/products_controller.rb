810c043d39a46dc0013f23e2e63d42dc3bd6e5d3
Chris Bedoya <christian.bedoya@yahoo.com>
2015-09-20 16:58:44 -0400

    september 22, 2015

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,7 +2,7 @@ class ProductsController < ApplicationController
    before_action :set_product, only: [:show, :edit, :update, :destroy]
    
    def index
-      @products = Product.group('category, name')
+      @products = Product.order('category, name')
    end
 
   # GET /products/1
